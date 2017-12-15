eddc2e1feb22d713a2dad4ae4c4a2c084cb67e96
eelvi <drv331@gmail.com>
2017-12-15 03:13:21 +0300

    fixed indentation and corrected some comments.

--- src/cstringo.c
+++ src/cstringo.c
@@ -25,73 +25,73 @@
 
 // right ( or reverse ) find, returns the idx of the first char of s1 that's in s2
 // before the third argument, if no match is found it returns STR_NPOS (-1)
-int rfindin( const char* s1, const char* s2, int before )
+int rfindin( const char *s1, const char *s2, int before )
 {
-        if(!s1 || !s2 )
-            return -1;
-        int s1len=strlen(s1);
-        int s2len=before;
-        if(s1len>s2len || !before)
-            return STR_NPOS;
-        for (int i=s2len-s1len; i>(0-1); i--){
-                if (strncmp(s1,s2+i,s1len)==0){
-                    return i;
-                }
-        }
+    if(!s1 || !s2 )
+        return -1;
+    int s1len = strlen(s1);
+    int s2len = before;
+    if(s1len > s2len || !before)
         return STR_NPOS;
+    for (int i = s2len-s1len; i > (0-1); i--) {
+        if (strncmp(s1, s2 + i, s1len) == 0) {
+            return i;
+        }
+    }
+    return STR_NPOS;
 }
 
 //reverse find starting from the end of the string, returns the index or STR_NPOS.
-int rfind( const char* s1, const char* s2)
+int rfind( const char *s1, const char *s2)
 {
 
-   return rfindin( s1, s2, strlen(s2) );
+    return rfindin( s1, s2, strlen(s2) );
 
 }
 // left (or normal) find, returns the idx of the first char of s1 that's in s2
 // before the third argument, if no match is found it returns STR_NPOS (-1)
-int lfindin( const char* s1, const char* s2, int before )
+int lfindin( const char *s1, const char *s2, int before )
 {
-        if(!s1 || !s2)
-            return -1;
-        int s1len=strlen(s1);
-        int s2len=before;
-        if(s1len>s2len)
-            return -1;
-        for (int i=0; i<s2len-s1len+1; i++){
-                if (strncmp(s1,s2+i,s1len)==0){
-                    return i;
-                }
-        }
+    if(!s1 || !s2)
         return -1;
+    int s1len = strlen(s1);
+    int s2len = before;
+    if(s1len > s2len)
+        return -1;
+    for (int i = 0; i < s2len-s1len +1; i++) {
+        if (strncmp(s1, s2 + i, s1len) == 0) {
+            return i;
+        }
+    }
+    return -1;
 
 }
 
 //normal find starting from the beginning of the string, returns the index or STR_NPOS.
-int lfind( const char* s1, const char* s2 )
+int lfind( const char *s1, const char *s2 )
 {
     return lfindin (s1, s2, strlen(s2));
 }
 
 //returns a new malloced string starting from the index begin up to and not including the index end.
 // returns a pointer to the new string or NULL.
-char *strcut(const char* str, int begin, int end)
+char *strcut(const char *str, int begin, int end)
 {
-    int len=end-begin+1;
-    if (!str || begin<0 || begin>end || strlen(str)<end )
+    int len = end-begin+ 1;
+    if (!str || begin < 0 || begin > end || strlen(str) < end )
         return NULL;
-    char* cutstr = malloc( len*sizeof(char) );
-    strncpy(cutstr, str+begin, len );
-    cutstr[len-1]='\0';
+    char *cutstr = malloc( len * sizeof(char) );
+    strncpy(cutstr, str+ begin, len );
+    cutstr[len - 1] = '\0';
     return cutstr;
 
 }
 //copies the string until end-1 then makes null terminates the string at destination
 // for example: to copy five from "fivefour" you call string_copy(dest,str,0,4); (the index of the charecter to be null)
 // and you need a char[ 4 + 1 ] buffer (end-begin + 1)
-int string_copy (char* dest, const char* str, int begin, int end)
+int string_copy (char *dest, const char *str, int begin, int end)
 {
-    if(!dest || !str || begin>end )
+    if(!dest || !str || begin > end )
         return 1;
     while ( *str && end-- )
         *(dest++) = *(str++);
@@ -106,25 +106,25 @@ int string_erase(char *str, int begin_idx, int end_idx)
 {
     if (!str || begin_idx > end_idx )// || strlen(str)<(begin_idx+end_idx)  )
         return -1;
-    char* old_beg = str+begin_idx;
-    char* new_beg = str+end_idx+1;
-    while ( *new_beg ){
+    char *old_beg = str + begin_idx;
+    char *new_beg = str + end_idx + 1;
+    while ( *new_beg ) {
         *(old_beg++) = *(new_beg++);
     }
-    *old_beg='\0';
-    
-    return (int) (old_beg-str); //new length
+    *old_beg = '\0';
+
+    return (int) (old_beg - str); //new length
 }
 //dest has to be as big as src for worst case scenarios and has to be null terminated
 int delimited_section_cpy (char *dest, const char *src, const char *delim)
 {
-    int end=lfind(delim,src);
-    if (end<STR_NPOS){
+    int end = lfind(delim, src);
+    if (end < STR_NPOS) {
         return 1;
     }
-    else if (end==STR_NPOS)
-        strcpy(dest,src);
-    else{
+    else if (end == STR_NPOS)
+        strcpy(dest, src);
+    else {
         if ( string_copy(dest, src, 0, end) != 0 )
             return 1;
     }
@@ -132,107 +132,111 @@ int delimited_section_cpy (char *dest, const char *src, const char *delim)
     return 0;
 }
 //rv0: failed rv[n]: n is the index of the first char of next line
-//when there isn't any it returns STR_NPOS (-1)
-int nextlinepos( const char* str, const char* delim)
+//when there isn't any it returns the index of \0
+int nextlinepos( const char *str, const char *delim)
 {
 
     int d_idx = 0;
-    if (!str || !delim || *str=='\0'  ) 
+    if (!str || !delim || *str == '\0'  )
         return STR_NPOS;
 
-    d_idx= lfind( delim, str );
-    if (d_idx == STR_NPOS){
+    d_idx = lfind( delim, str );
+    if (d_idx == STR_NPOS) {
         return strlen(str);
     }
 
-    return d_idx+strlen(delim);
+    return d_idx + strlen(delim);
 
 }
-//returns where the current line ends (when used with delim="\n" it returns the idx of \n or \0)
-int lineendpos( const char* str, const char* delim)
+//returns where the current line ends (when used with delim="\n"
+//it returns the idx of \n or the idx of \0)
+int lineendpos( const char *str, const char *delim)
 {
     int nextline_idx = nextlinepos(str, delim);
     if (!str[nextline_idx])
         return nextline_idx;
     else
-        return nextline_idx-strlen(delim);
+        return nextline_idx - strlen(delim);
 }
 //returns occurences of pat in str
 //(starting at 0) without considering overlaps, when there is an error it returns STR_NPOS
-int occurence_count(const char* str, const char *pat)
+int occurence_count(const char *str, const char *pat)
 {
     if (!str || !pat)
         return STR_NPOS;
-    int count=0;
+    int count = 0;
     int pat_len = strlen(pat);
 
-    while ( *str  ){
-        str+=nextlinepos(str, pat)+pat_len;
+    while ( *str  ) {
+        str += nextlinepos(str, pat) + pat_len;
         count++;
     }
-        
-return count;
+
+    return count;
 }
 
 //returns occurences of pat in str
-//(starting at 1) without considering overlaps, when there is an error it returns STR_NPOS 
-int sections_count(const char* str, const char *delim)
+//(starting at 1) without considering overlaps, when there is an error it returns STR_NPOS
+int sections_count(const char *str, const char *delim)
 {
-    int occ = occurence_count(str, delim)+1;
-    occ = (occ>=0) ? occ+1 : occ;
+    int occ = occurence_count(str, delim) + 1;
+    occ = (occ >= 0) ? occ + 1 : occ;
     return occ;
 }
 
 
 
 //this function replaces the string pat with the string repl and returns a new malloced string
-//with the resulting replacements, on failure it returns NULL, 
-char *string_cpy_repl(const char* src, const char* pat, const char* repl){
+//with the resulting replacements, on failure it returns NULL,
+char *string_cpy_repl(const char *src, const char *pat, const char *repl)
+{
     if (!src || !pat || !repl)
         return NULL;
     int pat_len = strlen(pat);
     int repl_len = strlen(repl);
     int old_len = strlen(src);
-    int new_len = old_len+repl_delta(src,pat,repl);
+    int new_len = old_len + repl_delta(src, pat, repl);
     char *new_string = malloc( new_len );
-    
+
     const char *tmp_src_ptr = src;
     char *tmp_dest_ptr = new_string;
-    while ( *tmp_src_ptr ){
-        if ( strncmp(tmp_src_ptr,pat,pat_len)==0 ){
-            strcpy(tmp_dest_ptr,repl);
-            tmp_dest_ptr+=repl_len;
-            tmp_src_ptr+=pat_len;
+    while ( *tmp_src_ptr ) {
+        if ( strncmp(tmp_src_ptr, pat, pat_len) == 0 ) {
+            strcpy(tmp_dest_ptr, repl);
+            tmp_dest_ptr += repl_len;
+            tmp_src_ptr += pat_len;
         }
-        else{
-                *(tmp_dest_ptr++) = *(tmp_src_ptr++);
+        else {
+            *(tmp_dest_ptr++) = *(tmp_src_ptr++);
         }
     }
-    new_string[new_len]='\0';
+    new_string[new_len] = '\0';
     return new_string;
-    
+
 }
 
 
 // this functions replcaes the string pat with the string repl
 // you're responsible for making sure the buffer fits, can return str_npos on failure.
 // otherwise it returns the number of replacements that occured.
-int str_repl( char* dest, const char* pat, const char* repl){
+int str_repl( char *dest, const char *pat, const char *repl)
+{
     int occ = occurence_count(dest, pat);
-    char* tmp = string_cpy_repl(dest,pat,repl);
-    if (tmp==NULL)
+    char *tmp = string_cpy_repl(dest, pat, repl);
+    if (tmp == NULL)
         return -1;
-    strcpy(dest,tmp);
+    strcpy(dest, tmp);
     free(tmp);
 
-    return occ;    
-} 
+    return occ;
+}
 // if strlen(s1) > strlen(s2) when using str_repl function, you are responsible that there is
-//  a big enough buffer, you can use this function  to show how many more (or less) 
+//  a big enough buffer, you can use this function  to show how many more (or less)
 //  bytes the string will have after replacement
 
-int repl_delta(const char* src, const char* pat, const char* repl){
-    
+int repl_delta(const char *src, const char *pat, const char *repl)
+{
+
     int occ = occurence_count(src, pat);
     int delta = ( occ * strlen(repl) ) - ( occ * strlen(pat) );
     return delta;
@@ -240,20 +244,19 @@ int repl_delta(const char* src, const char* pat, const char* repl){
 
 //returns a newly malloced string with the content up to the delimter string,
 //(can be used to seperate lines for example with delim="\n"), returns NULL on failure
-char* section_dup(const char* src, const char* delim)
+char *section_dup(const char *src, const char *delim)
 {
     if (!src || !delim || !*src)
         return NULL;
-    
+
     int len = lineendpos(src, delim);
     //printf("%d..\n", len);
     char *buff = malloc(len);
     //if (delimited_section_cpy(buff, src, delim) !=0 )
-    if (string_copy(buff, src, 0, len) !=0 )
-    {   
+    if (string_copy(buff, src, 0, len) != 0 ) {
         printf("fld");
         free(buff);
-        buff=NULL;
+        buff = NULL;
     }
 
     return buff;
@@ -261,27 +264,28 @@ char* section_dup(const char* src, const char* delim)
 
 //erases the white space at the beginning and end of the string, returns the number of
 //chars removed.
-int str_strip( char* dest ){
+int str_strip( char *dest )
+{
     if (!dest)
         return -1;
-    
+
     int len = strlen(dest);
-    int begin=0, end=len;
-    while ( end>0 && isspace(dest[end-1]) )
+    int begin = 0, end = len;
+    while ( end > 0 && isspace(dest[end - 1]) )
         end--;
     if (end != len)
-        string_erase(dest, end, len-1);
-    
-    if ( end!=0 && isspace(dest[begin]) ){
-        
-        while ( isspace(dest[begin+1]) )
+        string_erase(dest, end, len - 1);
+
+    if ( end != 0 && isspace(dest[begin]) ) {
+
+        while ( isspace(dest[begin + 1]) )
             begin++;
-        
-        string_erase(dest, 0, begin);   
-    
+
+        string_erase(dest, 0, begin);
+
     }
     //removed chars
-    return 0+begin+(len-end);
-    
+    return 0 + begin + (len - end);
+
 }
 
--- src/cstringo.h
+++ src/cstringo.h
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
- 
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,36 +25,36 @@
 
 // when a string isn't found or a general error occured this value is returned.
 #define STR_NPOS -1
-    
-    
-    //Functions that return new malloced strings require you to free the strings after using them.
-    //Some functions such as str_repl() require you to make sure you have a large enough buffer.
-    
+
+
+//Functions that return new malloced strings require you to free the strings after using them.
+//Some functions such as str_repl() require you to make sure you have a large enough buffer.
+
 
 
 // right ( or reverse ) find, returns the idx of the first char of s1 that's in s2
 // before the third argument, if no match is found it returns STR_NPOS (-1)
-int rfindin( const char* s1, const char* s2, int before );
+int rfindin( const char *s1, const char *s2, int before );
 
 //reverse find starting from the end of the string, returns the index or STR_NPOS.
-int rfind( const char* s1, const char* s2);
+int rfind( const char *s1, const char *s2);
 
 // left (or normal) find, returns the idx of the first char of s1 that's in s2
 // before the third argument, if no match is found it returns STR_NPOS (-1)
-int lfindin( const char* s1, const char* s2, int before );
+int lfindin( const char *s1, const char *s2, int before );
 
 //normal find starting from the beginning of the string, returns the index or STR_NPOS.
-int lfind( const char* s1, const char* s2 );
+int lfind( const char *s1, const char *s2 );
 
 
 //returns a new malloced string starting from the index begin up to and not including the index end.
 // returns a pointer to the new string or NULL.
-char *strcut(const char* str, int begin, int end);
+char *strcut(const char *str, int begin, int end);
 
 //copies the string until end-1 then makes null terminates the string at destination
 // for example: to copy five from "fivefour" you call string_copy(dest,str,0,4); (the index of the charecter to be null)
 // and you need a char[ 4 + 1 ] buffer (end-begin + 1)
-int string_copy (char* dest, const char* str, int begin, int end);
+int string_copy (char *dest, const char *str, int begin, int end);
 
 //dest has to be as big as src for worst case scenarios and has to be null terminated
 // common use case: delimited_section_cpy(dest,src, "\n) or (,,"\r\n")
@@ -62,23 +62,23 @@ int delimited_section_cpy (char *dest, const char *src, const char *delim);
 
 //returns the next char after the delimiter, when there isn't any
 //it returns returns zero on failure.
-int nextlinepos( const char* str, const char* delim);
+int nextlinepos( const char *str, const char *delim);
 
 //returns where the current line ends (when used with delim="\n" it returns the idx of \n or the string length)
 //when there are no delimiters found
-int lineendpos( const char* str, const char* delim);
+int lineendpos( const char *str, const char *delim);
 
 //returns occurences of pat in str
 //(starting at 0) without considering overlaps, when there is an error it returns STR_NPOS
-int occurence_count(const char* str, const char *pat);
+int occurence_count(const char *str, const char *pat);
 
 //returns occurences of pat in str
-//(starting at 1) without considering overlaps, when there is an error it returns STR_NPOS 
-int sections_count(const char* str, const char *delim);
+//(starting at 1) without considering overlaps, when there is an error it returns STR_NPOS
+int sections_count(const char *str, const char *delim);
 
 //returns a newly malloced string with the content up to the delimter string,
 //(can be used to seperate lines for example with delim="\n"), returns NULL on failure
-char* section_dup(const char* src, const char* delim);
+char *section_dup(const char *src, const char *delim);
 
 //erases the charecters in the range, including the charecter at end_idx, guarentees a '\0' at the new string end
 //  given the src was a proper string.
@@ -87,21 +87,21 @@ char* section_dup(const char* src, const char* delim);
 int string_erase(char *str, int begin_idx, int end_idx);
 
 //this function replaces the string pat with the string repl and returns a new malloced string
-//with the resulting replacements, on failure it returns NULL, 
-char *string_cpy_repl(const char* src, const char* pat, const char* repl);
+//with the resulting replacements, on failure it returns NULL,
+char *string_cpy_repl(const char *src, const char *pat, const char *repl);
 
 // this functions replcaes the string pat with the string repl
 // you're responsible for making sure the buffer fits, can return str_npos on failure.
 // otherwise it returns the number of replacements that occured.
-int str_repl( char* dest, const char* pat, const char* repl);
-    
+int str_repl( char *dest, const char *pat, const char *repl);
+
 // if strlen(s1) > strlen(s2) when using str_repl function, you are responsible that there is
-//  a big enough buffer, you can use this function  to show how many more (or less) 
+//  a big enough buffer, you can use this function  to show how many more (or less)
 //  bytes the string will have after replacement
-int repl_delta(const char* src, const char* pat, const char* repl);
+int repl_delta(const char *src, const char *pat, const char *repl);
 
 
 //erases the white space at the beginning and end of the string, returns the number of
 //chars removed.
-int str_strip( char* dest );
+int str_strip( char *dest );
 
